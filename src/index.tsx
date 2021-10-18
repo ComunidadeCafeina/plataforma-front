acd443f3fa71924ebcf1a3c72deaac99b4f2c21c
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-18 17:43:19 -0300

    install styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './global-styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles>
+      <App />
+    </GlobalStyles>
   </React.StrictMode>,
   document.getElementById('root'),
 );
