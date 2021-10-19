ead49b68a65dced03e2933a9357bf3b288db223b
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-19 01:01:00 -0300

    init 'Conheca nossos projetos' section

---
+++ src/pages/landing-page.style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const IconBox = styled.div`
+  min-width: 52px;
+`;
+
+export const Description = styled.div`
+  display: flex;
+  font-size: 20px;
+`;
--- src/pages/landing-page.tsx
+++ src/pages/landing-page.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { Icon, ContentContainer, Title } from '../components';
+import { IconBox, Description } from './landing-page.style';
+import { Card } from '../components/card';
+import projects from './projects';
 
-const LandingPage = (): React.ReactElement => <p>Compiladoras de Cafeína</p>;
+const LandingPage = (): React.ReactElement => (
+  <ContentContainer>
+    <Title>Conheça nossos projetos</Title>
+
+    <Description>
+      <IconBox>
+        <Icon icon="cadeado" />
+      </IconBox>
+      Alguns projetos são exclusivos para pessoas que fazem parte da comunidade
+    </Description>
+
+    {projects.map(project => (
+      <Card key={project.brandIcon} {...project} />
+    ))}
+  </ContentContainer>
+);
 
 export default LandingPage;
