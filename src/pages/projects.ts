9840716c7acf7d66f3fc5a6b2d3198f95758aba4
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-19 01:00:06 -0300

    add projects constant

---
+++ src/pages/projects.ts
@@ -0,0 +1,45 @@
+import { Icons } from '../components';
+
+const projects = [
+  {
+    brandIcon: 'comunidade' as Icons,
+    title: 'Comunidade Compiladoras',
+    description: 'Comunidade de apoio e engajamento',
+    icon: 'cadeado-botao',
+    link: '',
+  },
+  {
+    brandIcon: 'vagas' as Icons,
+    title: 'Cafeína Vagas',
+    description:
+      'Canal do Telegram com vagas inclusivas e para iniciantes em TI',
+    icon: 'seta',
+    link: '',
+  },
+  {
+    brandIcon: 'central-programadora' as Icons,
+    title: 'Central da Programadora',
+    description:
+      'Servidor no Discord com diversos canais para ajudar no seu desenvolvimento profissional',
+    icon: 'cadeado-botao',
+    link: '',
+  },
+  {
+    brandIcon: 'central-compiladoras' as Icons,
+    title: 'Central Compiladoras',
+    description:
+      'Um canal no Telegram para ficar em dia com todas as notícias e avisos da comunidade',
+    icon: 'seta',
+    link: '',
+  },
+  {
+    brandIcon: 'literaria' as Icons,
+    title: 'Cafeína Literária',
+    description:
+      'Nosso clube do livro, com leitura coletiva e podcasts literários, via Telegram',
+    icon: 'seta',
+    link: '',
+  },
+];
+
+export default projects;
