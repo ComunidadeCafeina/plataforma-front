66d2b6167eef4abe16ba79aa0a511d8050470ef7
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-21 12:56:11 -0300

    feat: add global styles

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -5,9 +5,14 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     margin: 0;
+    padding: 0;
     font-family: 'Montserrat', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+  }
+
+  div {
+    box-sizing: border-box;
   }
 
   h1 {
