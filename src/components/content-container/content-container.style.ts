5a5c54651ec8feadde6d56d7cfbbb9055759c18b
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-21 12:56:41 -0300

    fix: content container styles to mobile screen

--- src/components/content-container/content-container.style.ts
+++ src/components/content-container/content-container.style.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
 export const ContentContainer = styled.div`
-  padding: 16px;
+  margin: 96px 32px;
+  max-width: 1120px;
+
+  @media screen and (max-width: 720px) {
+    margin: 48px 16px;
+  }
 `;
