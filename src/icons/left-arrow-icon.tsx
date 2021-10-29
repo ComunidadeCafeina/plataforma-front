2c5a63667eaf78b6babb0ebd7b56d6032490e59d
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-29 09:53:19 -0300

    fix: add icons return type

--- src/icons/left-arrow-icon.tsx
+++ src/icons/left-arrow-icon.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
-
-export function LeftArrowIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      width={17}
-      height={18}
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path
-        d="M16 9.25H1M8.5 16.75L1 9.25l7.5-7.5"
-        stroke="#FE393B"
-        strokeWidth={2}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}
+export const LeftArrowIcon = (
+  props: React.SVGProps<SVGSVGElement>,
+): JSX.Element => (
+  <svg
+    width={17}
+    height={18}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M16 9.25H1M8.5 16.75L1 9.25l7.5-7.5"
+      stroke="#FE393B"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
