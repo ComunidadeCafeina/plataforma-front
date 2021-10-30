4440e2eee94ef4e66d0c9c7170a611e621f90712
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-29 21:18:53 -0300

    feat: create description component

---
+++ src/components/description/description.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Description = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+`;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './content-container';
 export * from './title';
 export * from './card';
-export * from './button';
+export * from './card-button';
 export * from './carousel';
+export * from './description';
