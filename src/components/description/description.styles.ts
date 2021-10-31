e5cd14445d24384048545b58ffbd88da87f51857
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-31 12:34:55 -0300

    fix: remove extra spacing on mobile screen

--- src/components/description/description.styles.ts
+++ src/components/description/description.styles.ts
@@ -4,4 +4,5 @@ export const Description = styled.div`
   display: flex;
   align-items: center;
   font-size: 20px;
+  margin-bottom: 32px;
 `;
--- src/pages/landing-page/sections/projects/projects.style.ts
+++ src/pages/landing-page/sections/projects/projects.style.ts
@@ -5,7 +5,6 @@ export const IconBox = styled.div`
 `;
 
 export const CardsContainer = styled.div`
-  margin-top: 32px;
   @media screen and (max-width: 768px) {
     flex-direction: column;
     align-items: center;
