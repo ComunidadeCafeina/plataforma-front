a70ad959e9b4513f062130d54f2e1dc8e323bfb3
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-31 12:35:36 -0300

    feat: add social networks mock

---
+++ src/pages/landing-page/sections/social-networks/social-networks-mock.ts
@@ -0,0 +1,44 @@
+const socialNetworks = [
+  {
+    text: 'Instagram',
+    icon: 'instagram',
+    link: '',
+  },
+  {
+    text: 'Facebook',
+    icon: 'facebook',
+    link: '',
+  },
+  {
+    text: 'Twitter',
+    icon: 'twitter',
+    link: '',
+  },
+  {
+    text: 'YouTube',
+    icon: 'youtube',
+    link: '',
+  },
+  {
+    text: 'LinkedIn',
+    icon: 'linkedin',
+    link: '',
+  },
+  {
+    text: 'Podcast',
+    icon: 'podcast',
+    link: '',
+  },
+  {
+    text: 'Github',
+    icon: 'github',
+    link: '',
+  },
+  {
+    text: 'Blog',
+    icon: 'blog',
+    link: '',
+  },
+];
+
+export default socialNetworks;
