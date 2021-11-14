28cab913a369eee40b4cad90525a9d4093d06c19
IsabellaSoares <isabella.soares.28@gmail.com>
2021-11-14 18:07:24 -0300

    fix: import error

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Landing Page', async () => {
-  render(<App />);
-  const title = await screen.findByText('Compiladoras de Cafeína');
-  expect(title).toBeInTheDocument();
-});
