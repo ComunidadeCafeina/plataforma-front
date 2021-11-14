7668cdd40e016ed87092eb45d6568097b0ec8eae
IsabellaSoares <isabella.soares.28@gmail.com>
2021-11-14 15:09:31 -0300

    fix: App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Landing Page', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/compiladoras de cafeína/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = await screen.findByText(/compiladoras de cafeína/i);
+  expect(title).toBeInTheDocument();
 });
