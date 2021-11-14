9869d26996e28f554f8ceeaa98b0ee5350f2eb3f
IsabellaSoares <isabella.soares.28@gmail.com>
2021-11-14 17:41:47 -0300

    fix: remove jest timeout

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom';
 import { toHaveNoViolations } from 'jest-axe';
 
-jest.setTimeout(10000);
 expect.extend(toHaveNoViolations);
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders Landing Page', async () => {
   render(<App />);
-  const title = await screen.findByText(/compiladoras de cafeína/i);
+  const title = await screen.findByText('Compiladoras de Cafeína');
   expect(title).toBeInTheDocument();
 });
