f9cb751fddd89d746d942f1924d738d609887177
IsabellaSoares <isabella.soares.28@gmail.com>
2021-11-14 22:42:22 -0300

    chore: setup MSW

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,20 @@
 import '@testing-library/jest-dom';
 import { toHaveNoViolations } from 'jest-axe';
+import { server } from './src/mocks/server';
 
 expect.extend(toHaveNoViolations);
+
+beforeAll(() => {
+  // Enable the mocking in tests.
+  server.listen();
+});
+
+afterEach(async () => {
+  // Reset any runtime handlers tests may use.
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  // Clean up once the tests are done.
+  server.close();
+});
