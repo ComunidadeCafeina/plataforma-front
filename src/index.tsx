3747ef9ec85613954628ef201158f556dc949f28
IsabellaSoares <isabella.soares.28@gmail.com>
2021-12-13 15:27:11 -0300

    feat: setup GA

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GA4React from 'ga-4-react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import worker from './mocks/browser';
@@ -8,12 +9,18 @@ if (process.env.NODE_ENV === 'development') {
   worker.start();
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const ga4react = new GA4React('G-D64TDBEBXV');
+
+(async () => {
+  await ga4react.initialize();
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+  );
+})();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
